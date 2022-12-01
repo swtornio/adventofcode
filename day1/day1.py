21208902353f437c6fbb6007448f7fc71d705aba
Steve Tornio <swtornio@gmail.com>
2022-12-01 07:37:09 -0600

    complete part 2

--- day1/day1.py
+++ day1/day1.py
@@ -4,9 +4,12 @@
 # Add up calories for each elf, separated by blank lines
 # Store as highest number, if higher than previous
 # Submit total for elf with most calories
+#
+# Part 2 - report total of top 3 elves
 
 highest = 0
 current = 0
+calorie_loads = list()
 
 with open("input.txt", "r") as f:
     for line in f:
@@ -18,7 +21,13 @@ with open("input.txt", "r") as f:
             # empty line means check if current is highest
             if current > highest:
                 highest = current
+            calorie_loads.append(current)
             current = 0
-            lines = 0
+
+# Sort the list of calorie loads in place
+calorie_loads.sort(reverse=True)
 
 print(f"The most calories held by an elf is {highest}.")
+print(f"The top 3 calorie loads are {calorie_loads[0]}, {calorie_loads[1]}, and {calorie_loads[2]}.")
+top_three_sum = calorie_loads[0] + calorie_loads[1] + calorie_loads[2]
+print(f"The total calories of the top three is {top_three_sum}.")
